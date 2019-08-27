0c909ff45449631a6f5df14d820b39b03ca5638f
zhouxiang <zhouxiang@jntci.com.cn>
2019-08-27 14:47:02 +0800

    1

--- src/zel.js
+++ src/zel.js
@@ -1,9 +1,80 @@
+class Eventable{
+  constructor(){
+    this[this.PROPERTY.EVENT_HANDLERS] = {};
+
+    this._bindEvent();
+  }
+
+  on(type, handler){
+    if( !this[this.PROPERTY.EVENT_HANDLERS][type] ){
+      this[this.PROPERTY.EVENT_HANDLERS][type] = [];
+    }
+    this[this.PROPERTY.EVENT_HANDLERS][type].push(handler);
+    return this;
+  }
+
+  trigger(type, e){
+    if( this[this.PROPERTY.EVENT_HANDLERS][type] &&
+    this[this.PROPERTY.EVENT_HANDLERS][type].length !== 0 ){
+      for( let handler of this[this.PROPERTY.EVENT_HANDLERS][type] ){
+        handler.call(this, e);
+      }
+    }
+
+    if( this[this.PROPERTY.EVENT_HANDLERS][`${type}$asyn`] &&
+    this[this.PROPERTY.EVENT_HANDLERS][`${type}$asyn`].length !== 0 ){
+      for( let handler of this[this.PROPERTY.EVENT_HANDLERS][`${type}$asyn`] ){
+        window.setTimeout(()=>{
+          handler.call(this, e);
+        })
+      }
+    }
+  }
+
+  _bindEvent(){
+    let proto = this.__proto__;
+    while( proto !== Object.prototype){
+      for( let type of Object.getOwnPropertyNames(proto) ){
+        if( type.startsWith('on') && type !== 'on'){
+          this.on( type.substr(2), this[type].bind(this) );
+        }
+      }
+      proto = proto.__proto__;
+    }
+  }
+
+  get PROPERTY(){
+    return {
+      EVENT_HANDLERS: '[EventHandlers]'
+    }
+  }
+}
+
+class Part extends Eventable{
+  constructor(){
+    super();
+
+  }
+
+  onload(){
+    console.log( this instanceof Part );
+  }
+}
+
+
 class Zel{
   constructor(){
     this._dom = null;
 
     this._lineNum = null;
     this._editArea = null;
+
+    this._fontStyle = {
+      fontSize : '12px',
+      fontFamily : 'monospace,Hiragino Sans GB,STHeiti,Microsoft Yahei,sans-serif',
+      fontColor : 'white'
+    };
+
   }
 
   init( dom , file = null){
@@ -13,11 +84,15 @@ class Zel{
       this._dom = document.getElementById(dom);
     }
 
-    let isty = this._dom.style;
-    isty.position = 'relative';
+    let is = this._dom.style;
+    is.position = 'relative';
+    is.fontFamily = this._fontStyle.fontFamily || 'Microsoft Yahei';
+    is.color = this._fontStyle.fontColor || 'black';
+    is.fontSize = this._fontStyle.fontSize || '10px';
 
-    if( isty.backgroundColor === "" )
-      isty.backgroundColor = '#9e9e9e';
+
+    if( is.backgroundColor === "" )
+      is.backgroundColor = '#9e9e9e';
     
     this._bindevent();
     this._initLineNumber();
@@ -75,16 +150,34 @@ class ZInputContext{
     istyle.border = 'none';
     istyle.backgroundColor = 'rgba(0,0,0,0)';
     istyle.width = '100%';
+
+    this._bindevent();
   }
 
   focus(){
     this._dom.focus();
   }
 
+
+
   _bindevent(){
     for( let en in document ){
       if( en.startsWith("on") && this[en]){
         this._dom.addEventListener( en.substr(2), this[en].bind(this) );
+      }
+    }
+    this._bindAlle();
+  }
+
+  _bindAlle(){
+    for( let en in document ){
+      if( en.startsWith("on") ){
+        this._dom.addEventListener( en.substr(2), function (e){
+          
+          console.log( `type:${e.type},value:${e.type === 'input'?e.data:'null'}` );
+  
+          console.log(e);
+        });
       }
     }
   }
@@ -129,6 +222,7 @@ class ZEditArea{
     ls.height = '100%';
     
     ls.display = 'inline-block';
+    ls.position = 'relative';
 
     this._bindevent();
   }
